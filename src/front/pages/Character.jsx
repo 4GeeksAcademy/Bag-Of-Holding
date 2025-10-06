071475f87a37661f404da165168fc0b07d1a7a24
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-10-06 00:23:15 +0000

    Set up stat block example values to use as example for the models

--- src/front/pages/Character.jsx
+++ src/front/pages/Character.jsx
@@ -63,22 +63,22 @@ export const Character = () => {
                 <img src="https://i.pinimg.com/236x/aa/00/72/aa0072133c6bb0e1d968052509292a89.jpg" className="character-picture m-3"></img>
 
                 {/* CHARACTER NAME */}
-                <div className="character-info-box rounded m-3">
+                <div className="character-info-box m-3">
                     <h5>MALEKITH THE WICKED</h5>
                 </div>
 
                 {/* CHARACTER RACE */}
-                <div className="character-info-box rounded m-3">
+                <div className="character-info-box m-3">
                     <h5>HIGH ELF</h5>
                 </div>
 
                 {/* CHARACTER CLASS */}
-                <div className="character-info-box rounded m-3">
+                <div className="character-info-box m-3">
                     <h5>WIZARD</h5>
                 </div>
 
                 {/* CHARACTER SUBCLASS */}
-                <div className="character-info-box rounded m-3">
+                <div className="character-info-box m-3">
                     <h5>RUNECRAFTER</h5>
                 </div>
             </div>
@@ -86,17 +86,17 @@ export const Character = () => {
                 <div className="row info-box rounded h-50 mb-4">
                     {/* THIS WILL BE A MAP OF A LIST CONTAINING THE INFORMATION ON ALL STATS*/}
                     <div className="row m-auto">
-                    {
-                        stats
-                            ? stats.map((stat, index) => {
-                                return (
-                                    <div className="col-2" key={index}>
-                                        <StatBlock name={stat.name} value={stat.value} check={stat.check} saving={stat.saving}/>
-                                    </div>
-                                )
-                            })
-                            : ""
-                    }
+                        {
+                            stats
+                                ? stats.map((stat, index) => {
+                                    return (
+                                        <div className="col-2" key={index}>
+                                            <StatBlock name={stat.name} value={stat.value} check={stat.check} saving={stat.saving} />
+                                        </div>
+                                    )
+                                })
+                                : ""
+                        }
                     </div>
                 </div>
                 <div className="row h-50">
