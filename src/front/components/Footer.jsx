5f0bcd8566b7addd12edd5040375265e3783dec0
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-10-16 21:07:23 +0000

    Updated names of group members in footer.

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
 	<footer className="footer header-secondary mt-5 py-3 text-center accent">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{"Ricardo & Lilith"}
+			Made with <i className="fa fa-heart text-danger" /> by{" Ricardo, Lilith, & Sean"}
 		</p>
 	</footer>
 );
